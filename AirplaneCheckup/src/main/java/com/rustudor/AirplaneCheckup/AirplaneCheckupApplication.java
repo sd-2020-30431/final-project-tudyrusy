135923f3e240a8ef03840dd8cf029fd9ac5c20e5
Rus <tudy.rusy@gmail.com>
2020-05-28 02:28:11 +0300

    start

---
+++ AirplaneCheckup/src/main/java/com/rustudor/AirplaneCheckup/AirplaneCheckupApplication.java
@@ -0,0 +1,13 @@
+package com.rustudor.AirplaneCheckup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirplaneCheckupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirplaneCheckupApplication.class, args);
+	}
+
+}
